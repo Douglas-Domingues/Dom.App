6c0e5b016c30b7644aa7e9e30c66cb9500a6e92e
Douglas Domingues <domingues.douglas95@gmail.com>
2024-10-08 20:02:35 -0300

    Theme Mode dinâmico Alteração para pegar o tema do site baseando-se no tema do computador.

---
+++ Dom.App/Configuration.cs
@@ -0,0 +1,36 @@
+﻿using MudBlazor;
+
+namespace Dom.App;
+
+public static class Configuration
+{
+    public static MudTheme Theme = new()
+    {
+        Typography = new Typography
+        {
+            Default = new Default
+            {
+                FontFamily = ["Comfortaa", "sans-serif"]
+            }
+        },
+        PaletteLight = new PaletteLight
+        {
+            Primary = new MudBlazor.Utilities.MudColor("#118df0"),
+            Secondary = Colors.Indigo.Darken3,
+            Background = Colors.Gray.Lighten4,
+            AppbarBackground = new MudBlazor.Utilities.MudColor("#118df0"),
+            AppbarText = Colors.Shades.Black,
+            TextPrimary = Colors.Shades.Black,
+            DrawerText = Colors.Shades.Black,
+            DrawerBackground = Colors.LightBlue.Lighten4
+
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = Colors.LightBlue.Accent3,
+            Secondary = Colors.LightBlue.Darken3,
+            AppbarBackground = Colors.LightBlue.Accent3,
+            AppbarText = Colors.Shades.Black
+        }
+    };
+}
